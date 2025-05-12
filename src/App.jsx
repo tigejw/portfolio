1873d202a208f0e458aedd373c7079513f87bf7c
tigejw <tigejw@gmail.com>
2025-05-12 10:02:53 +0100

    initial commit. setup basic css from figma design. no functionality but responsive design.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="desktop-container">
+        <div className="rectangle-base">
+          <div className="rectangle-top-depth"></div>
+          <div className="rectangle-screen"></div>
+          <div className="rectangle-dvd-under"></div>
+          <div className="rectangle-dvd-over"></div>
+          <div className="button-dark-mode"></div>
+          <div className="button-mute"></div>
+          <div className="button-eject"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
