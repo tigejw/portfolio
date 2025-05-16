8d93a2bd66e265e7c2516813632cc214cee79068
tigejw <tigejw@gmail.com>
2025-05-16 13:15:41 +0100

    install howler. get basic backing track working on click

--- src/componants/Desktop.jsx
+++ src/componants/Desktop.jsx
@@ -1,6 +1,22 @@
 import ButtonGrid from "./ButtonGrid"
-
+import { useEffect, useRef } from "react";
+import { Howl, Howler } from "howler";
 export default function Desktop({dispatch}){
+
+  const minecraftRef = useRef(null);
+
+  useEffect(() => {
+    minecraftRef.current = new Howl({
+      src: ["/audio/1-03. Subwoofer Lullaby.mp3"],
+      loop: true,
+    });
+    Howler.volume(0.5);
+  }, []);
+
+  const startAudio = () => {
+    minecraftRef.current?.play();
+  };
+
     return(
         <div className="desktop-container">
         <div className="rectangle-base">
@@ -12,7 +28,9 @@ export default function Desktop({dispatch}){
           <div className="rectangle-dvd-under"></div>
           <div className="rectangle-dvd-over"></div>
           <div className="button-dark-mode"></div>
-          <div className="button-mute"></div>
+          <div className="button-mute" onClick={startAudio}>
+m
+          </div>
           <div className="button-eject"></div>
         </div>
       </div>
